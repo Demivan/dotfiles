65e5f90e4bcee3d3b66ed7957ef002f1b7922e09
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-01-18 18:17:15 +0200

    Use custom kernel

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -12,7 +12,9 @@
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
+  boot.kernelPackages = pkgs.linuxPackages_cachyos-sched-ext;
   boot.kernelModules = ["kvm-amd"];
+  environment.systemPackages =  [ pkgs.scx ];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
