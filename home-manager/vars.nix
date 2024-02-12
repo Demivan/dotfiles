bce13d454c29bc8a9cfff16c46d898e2d94047b6
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-02-12 22:25:42 +0200

    Run formatter

--- home-manager/vars.nix
+++ home-manager/vars.nix
@@ -19,19 +19,19 @@
     overlay1 = "#7f849c";
     overlay2 = "#9399b2";
 
-    blue      = "#89b4fa";
-    lavender  = "#b4befe";
-    sapphire  = "#74c7ec";
-    sky       = "#89dceb";
-    teal      = "#94e2d5";
-    green     = "#a6e3a1";
-    yellow    = "#f9e2af";
-    peach     = "#fab387";
-    maroon    = "#eba0ac";
-    red       = "#f38ba8";
-    mauve     = "#cba6f7";
-    pink      = "#f5c2e7";
-    flamingo  = "#f2cdcd";
+    blue = "#89b4fa";
+    lavender = "#b4befe";
+    sapphire = "#74c7ec";
+    sky = "#89dceb";
+    teal = "#94e2d5";
+    green = "#a6e3a1";
+    yellow = "#f9e2af";
+    peach = "#fab387";
+    maroon = "#eba0ac";
+    red = "#f38ba8";
+    mauve = "#cba6f7";
+    pink = "#f5c2e7";
+    flamingo = "#f2cdcd";
     rosewater = "#f5e0dc";
 
     blueHypr = "rgb(89b4fa)";
