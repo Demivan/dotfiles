2c45a3fc35c464233ab47d7351eef057f1f9a91b
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-03-03 09:50:11 +0200

    Fix Slack integration and add Wayland support

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -53,6 +53,18 @@
           patches = [
           ];
         });
+
+      slack = prev.slack.overrideAttrs (old: {
+        fixupPhase = ''
+          sed -i -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
+
+          rm $out/bin/slack
+          makeWrapper $out/lib/slack/slack $out/bin/slack \
+            --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+            --suffix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
+            --add-flags "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations,WebRTCPipeWireCapturer"
+        '';
+      });
     })
   ];
 
