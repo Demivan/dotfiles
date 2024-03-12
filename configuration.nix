9635864fba2accbd5c43a845019a9a83f4f60070
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-03-12 18:31:00 +0200

    allow to mount removable media

--- configuration.nix
+++ configuration.nix
@@ -136,6 +136,10 @@
 
   programs.fish.enable = true;
 
+  services.devmon.enable = true;
+  services.gvfs.enable = true; 
+  services.udisks2.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
